3e852680a0a910c04500df011be7bd2171a3943f
Ruan <santosruan2021@gmail.com>
2026-04-06 18:09:40 -0300

    feat: beautify and finish Connections Status sheet

--- lib/app/ui/home/widgets/status_panel_widget.dart
+++ lib/app/ui/home/widgets/status_panel_widget.dart
@@ -7,7 +7,6 @@ import 'package:smart_stock/app/domain/status_color.dart';
 import 'package:smart_stock/app/ui/_core/providers/ble_connection_provider.dart';
 import 'package:smart_stock/app/ui/_core/providers/stock_provider.dart';
 import 'package:smart_stock/app/ui/_core/widgets/custom_card.dart';
-import 'package:smart_stock/app/ui/_core/widgets/update_stock_btn.dart';
 import 'package:smart_stock/app/ui/home/widgets/status_sheet_widget.dart';
 
 class StatusPanelWidget extends ConsumerWidget {
@@ -16,9 +15,9 @@ class StatusPanelWidget extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final bleState = ref.watch(bleConnectionProvider.select((state) => state.currentState));
-    final stockState = ref.watch(stockProvider);
-
     final bleStatusColor = getBleStatusColor(bleState);
+
+    final stockState = ref.watch(stockProvider);
     final stockStatusColor = getStockStatusColor(
       stockState,
       ref.watch(stockProvider.notifier).updatedAt,
@@ -28,41 +27,26 @@ class StatusPanelWidget extends ConsumerWidget {
 
     return InkWell(
       onTap: () {
-        showStatusSheet(
-          context,
-          bleStatusColor: bleStatusColor,
-          stockStatusColor: stockStatusColor,
-          connectedDevice: bleState.manager.connectedPistol,
-        );
-        
+        showStatusSheet(context);
       },
       child: CustomCard(
-        child: Column(
-          spacing: 20,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
+            Text(
+              'Status da conexão',
+              textAlign: TextAlign.start,
+              style: typography.base.copyWith(fontWeight: FontWeight.bold, color: Colors.black87),
+            ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              spacing: 8,
               children: [
-                Text(
-                  'Status da conexão',
-                  textAlign: TextAlign.start,
-                  style: typography.base.copyWith(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black87,
-                  ),
-                ),
-                Row(
-                  spacing: 8,
-                  children: [
-                    _StatusItem(color: bleStatusColor, height: 36, svgPath: Assets.scannerIcon),
-                    _StatusItem(svgPath: Assets.stockIcon, height: 32, color: stockStatusColor),
-                    const SizedBox(width: 2),
-                    const Icon(FIcons.chevronRight, size: 18),
-                  ],
-                ),
+                _StatusItem(color: bleStatusColor, height: 36, svgPath: Assets.scannerIcon),
+                _StatusItem(svgPath: Assets.stockIcon, height: 32, color: stockStatusColor),
+                const SizedBox(width: 2),
+                const Icon(FIcons.chevronRight, size: 18),
               ],
             ),
-            UpdateStockButton(),
           ],
         ),
       ),
--- lib/app/ui/home/widgets/status_sheet_widget.dart
+++ lib/app/ui/home/widgets/status_sheet_widget.dart
@@ -1,32 +1,19 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:forui/forui.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:smart_stock/app/config/assets.dart';
 import 'package:smart_stock/app/domain/status_color.dart';
 import 'package:smart_stock/app/routing/router.dart';
+import 'package:smart_stock/app/ui/_core/providers/ble_connection_provider.dart';
+import 'package:smart_stock/app/ui/_core/providers/stock_provider.dart';
 import 'package:smart_stock/app/ui/_core/theme/custom_forui.dart';
+import 'package:smart_stock/app/ui/_core/widgets/update_stock_btn.dart';
 import 'package:smart_stock/app/ui/inventory/session/widgets/inventory_modals_widgets.dart';
 
-Future<void> showStatusSheet(
-  BuildContext context, {
-  required BluetoothDevice? connectedDevice,
-  required Color bleStatusColor,
-  required Color stockStatusColor,
-}) async {
-  String bleStatusText = '';
-
-  if (bleStatusColor == StatusColors.ERROR.color) {
-    bleStatusText = 'Erro na conexão';
-  } else if (bleStatusColor == StatusColors.LOADING.color) {
-    bleStatusText = 'Buscando dispositivos...';
-  } else if (bleStatusColor == StatusColors.OK.color) {
-    bleStatusText = connectedDevice?.platformName ?? 'Conectado';
-  }
-
-  const String stockStatusText = '';
-
+Future<void> showStatusSheet(BuildContext context) async {
   return showFSheet(
     style: getModalBlurStyle(context).call,
     context: context,
@@ -35,48 +22,32 @@ Future<void> showStatusSheet(
       child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          // mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text(
-              'Status da conexão',
+              'Conexões',
               style: context.theme.typography.xl2.copyWith(fontWeight: FontWeight.bold),
             ),
             Expanded(
               child: Material(
-                child: ListView(
-                  padding: const EdgeInsets.all(8),
-                  children: <Widget>[
-                    ListTile(
-                      title: Text(bleStatusText, textAlign: TextAlign.start),
-                      leading: SvgPicture.asset(
-                        Assets.scannerIcon,
-                        height: 36,
-                        color: bleStatusColor,
-                      ),
-                      trailing: const Icon(FIcons.chevronRight, size: 18),
-                      onTap: () {
-                        context.router.push(const DevicesRoute());
-                      },
-                    ),
-                    ListTile(
-                      title: const Text('Entry A', textAlign: TextAlign.start),
-                      leading: SvgPicture.asset(
-                        Assets.stockIcon,
-                        height: 32,
-                        color: stockStatusColor,
-                      ),
-                    ),
-                  ],
+                child: DecoratedBox(
+                  decoration: const BoxDecoration(color: Colors.white),
+                  child: ListView(
+                    padding: const EdgeInsets.all(8),
+                    children: <Widget>[
+                      const BleListTile(),
+                      const StockListTile(),
+                      UpdateStockButton(),
+                    ],
+                  ),
                 ),
               ),
             ),
-
             FButton(
               style: createLargeStyle(
                 context: context,
-                backgroundColor: Colors.green,
-                foregroundColor: Colors.lightGreen,
+                backgroundColor: context.theme.colors.primary,
+                foregroundColor: context.theme.colors.primaryForeground,
               ),
               onPress: () {
                 Navigator.of(context).pop();
@@ -96,15 +67,67 @@ Future<void> showStatusSheet(
   );
 }
 
-class _StatusItem extends StatelessWidget {
-  final Color color;
-
-  final double height;
-  final String? svgPath;
-  const _StatusItem({required this.color, this.svgPath, required this.height});
+class BleListTile extends ConsumerWidget {
+  const BleListTile({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return SvgPicture.asset(svgPath!, height: height, color: color);
+  Widget build(BuildContext context, WidgetRef ref) {
+    final bleState = ref.watch(bleConnectionProvider.select((state) => state.currentState));
+    final bleStatusColor = getBleStatusColor(bleState);
+
+    final connectedDevice = bleState.manager.connectedPistol;
+
+    String bleStatusText = '';
+
+    if (bleStatusColor == StatusColors.ERROR.color) {
+      bleStatusText = 'Erro na conexão';
+    } else if (bleStatusColor == StatusColors.LOADING.color) {
+      bleStatusText = 'Buscando dispositivos...';
+    } else if (bleStatusColor == StatusColors.OK.color) {
+      bleStatusText = connectedDevice?.platformName ?? 'Conectado';
+    }
+
+    return ListTile(
+      title: Text(bleStatusText, textAlign: TextAlign.start),
+      leading: SvgPicture.asset(Assets.scannerIcon, height: 36, color: bleStatusColor),
+      trailing: const Icon(FIcons.chevronRight, size: 18),
+      onTap: () {
+        context.router.push(const DevicesRoute());
+      },
+    );
+  }
+}
+
+class StockListTile extends ConsumerWidget {
+  const StockListTile({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final stockState = ref.watch(stockProvider);
+    final stockStatusColor = getStockStatusColor(
+      stockState,
+      ref.watch(stockProvider.notifier).updatedAt,
+    );
+
+    return ListTile(
+      title: _buildLastUpdateInfo(context, ref),
+      leading: SvgPicture.asset(Assets.stockIcon, height: 32, color: stockStatusColor),
+    );
+  }
+
+  Widget _buildLastUpdateInfo(BuildContext context, WidgetRef ref) {
+    final updatedAt = ref.watch(stockProvider.notifier).updatedAt;
+
+    if (updatedAt == null) {
+      return const Text('Estoque ainda não atualizado');
+    }
+
+    final dateFormat = DateFormat('dd/MM').format(updatedAt);
+    final timeFormat = DateFormat('HH:mm').format(updatedAt);
+
+    final text = DateTime.now().difference(updatedAt).inDays < 1
+        ? timeFormat
+        : '$timeFormat, $dateFormat';
+    return Text('Atualizado às $text', textAlign: TextAlign.start);
   }
 }
